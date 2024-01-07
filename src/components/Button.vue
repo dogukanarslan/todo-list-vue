5ee2f664fb3260ffd9a0ae0c265281455041fb56
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-01-08 00:11:52 +0300

    Add danger button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,21 +1,47 @@
 <template>
-  <button class="btn" @click="$emit('click')">{{ label }}</button>
+  <button
+    class="btn"
+    :class="
+      color === 'primary'
+        ? 'btn-primary'
+        : color === 'danger'
+        ? 'btn-danger'
+        : ''
+    "
+    @click="$emit('click')"
+  >
+    {{ label }}
+  </button>
 </template>
 
 <script>
 export default {
   emits: ["click"],
-  props: ["label"]
+  props: {
+    label: {},
+    color: {
+      default() {
+        return "primary"
+      }
+    }
+  }
 }
 </script>
 
 <style>
 .btn {
   border: none;
-  background: var(--primary);
   color: white;
   padding: 0.5rem 0.8rem;
   border-radius: 0.25rem;
   cursor: pointer;
+}
+
+.btn-primary {
+  background-color: var(--primary);
+}
+
+.btn-danger {
+  background-color: var(--danger);
 }
 </style>
